a4bcb97b2c5ed8408bf09dfd2dd5a31b85d62ffb
aalurip0512 <parashar.a.a@gmail.com>
2021-04-09 16:20:00 -0400

    Selenium Tutorial

---
+++ src/chapter6/HomeAreaCalculator.java
@@ -0,0 +1,15 @@
+package chapter6;
+
+import java.awt.*;
+
+public class HomeAreaCalculator {
+    public static void main(String args[]){
+        /**
+         * Rectangle
+         */
+
+         //Create instance of rectangle
+
+        Rectangle room1 = new Rectangle();
+    }
+}
