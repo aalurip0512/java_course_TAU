30c05e0e83e718faef9a337aed99272a9cf155b9
ParasharAaluri <ParasharAaluri@DESKTOP-F0B8RV6>
2021-05-25 15:59:11 -0400

    TestNG

---
+++ src/testNG/NewTest.java
@@ -0,0 +1,43 @@
+package testNG;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
